a1b0dda5891d7ec8272441c0cefbab0a1c03995a
YEKabantsov <tesnus288@gmail.com>
2018-06-22 16:10:31 +0300

    Add files via upload
    init

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.get('/', function(request, response){
+	response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+	// console.log('Someome joined');
+	var user = Date.now();
+
+	socket.on('message.sent', function (message) {
+		io.emit('message', user + ': ' + message);
+		console.log(message);
+	});
+
+	io.emit('message', 'User ' + user + ' connected');
+	console.log('User ' + user + ' connected');
+});
+
+http.listen(3000, function(){
+	console.log('Start server');
+});
